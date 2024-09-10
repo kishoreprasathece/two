61b8dc12735cb6786ca82a0ef2291b4a303b15aa
kishoreprasathece <kishoreprasathece@gmail.com>
2024-09-10 21:31:58 +0530

    5

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-vio:"#739FE8",
-vioo:"#4F18CA",
-vv:"#4A16BD"
+vio:"#0E409E",
+vioo:"#7F56D9",
+vvt:"#6E53D0",
+vvf:"D9ECFF",
 
       },
       fontFamily:{
